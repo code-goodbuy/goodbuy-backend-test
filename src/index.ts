775bcb5fa6dbbec7c40cddbe91808b89bc7a7c45
jwdotpark <jwdotpark@me.com>
2021-03-05 20:58:24 +0100

    clean up code / logger connected

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,36 @@ import express from 'express';
 import cors from 'cors';
 import logging from './config/logging';
 
-// Variables
-// ========================================
 const router = express();
 const PORT = process.env.PORT || 1337;
 const VERSION = process.env.VERSION || '1.0.0';
 const ENVIRONMENT = process.env.NODE_ENV || 'development';
 const NAMESPACE = 'Server';
 
-// Configurations
-// ========================================
 router.use(cors());
 
-// Log request middleware
-// ========================================
 router.use((req, res, next) => {
-  // log req
+  // TODO need to be predefined on production
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
+router.use((req, res, next) => {
   logging.info(
     NAMESPACE,
     `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
   );
 
-  // listener when anything is finished
   res.on('finish', () => {
     logging.info(
       NAMESPACE,
@@ -33,14 +41,10 @@ router.use((req, res, next) => {
   next();
 });
 
-// Endpoints
-// ========================================
 router.get('/', (req, res) =>
   res.send({ version: VERSION, environment: ENVIRONMENT })
 );
 
-// Server
-// ========================================
 router.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 // TODO change main AWS account to one of IAM USER not main one

8b2507c09280cbe6a357dc86ec1c5f3bf44aa64d
jwdotpark <jwdotpark@me.com>
2021-03-05 20:28:16 +0100

    added logger

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,43 @@
-// Imports
-// ========================================
 import express from 'express';
 import cors from 'cors';
+import logging from './config/logging';
 
 // Variables
 // ========================================
-const app = express();
+const router = express();
 const PORT = process.env.PORT || 1337;
 const VERSION = process.env.VERSION || '1.0.0';
+const NAMESPACE = 'Server';
 
 // Configurations
 // ========================================
-app.use(cors());
+router.use(cors());
+
+// Log request middleware
+// ========================================
+router.use((req, res, next) => {
+  // log req
+  logging.info(
+    NAMESPACE,
+    `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+  );
+
+  // listener when anything is finished
+  res.on('finish', () => {
+    logging.info(
+      NAMESPACE,
+      `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`
+    );
+  });
+  next();
+});
 
 // Endpoints
 // ========================================
-app.get('/', (req, res) => res.send({ version: VERSION }));
+router.get('/', (req, res) => res.send({ version: VERSION }));
 
 // Server
 // ========================================
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+router.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+// TODO change main AWS account to one of IAM USER not main one
